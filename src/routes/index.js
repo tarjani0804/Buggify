f23d885777d9bd8d300afd9b1926a260a7ceea81
Tarjani <tarjanipatel.19.cs@iite.indusuni.ac.in>
2023-03-04 00:35:49 +0530

    console added forr checking commits

--- src/routes/index.js
+++ src/routes/index.js
@@ -254,4 +254,5 @@ app.post('/setScope', middleware, async (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server started on port 5173`);
+  console.log(`Server started on port 5173`);
 });
